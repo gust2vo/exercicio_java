cc7b07be4250926cc6c82d05ffa1136181a5a2ec
gust2vo <gustavocardosodealmeida1@gmail.com>
2024-03-20 20:16:53 -0300

    Criando classe

---
+++ data_string/data_string/src/Pessoa.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+
+public class Pessoa {
+    private String nome;
+    private String sobrenome;
+    private LocalDate data_nascimento;
+    private String email;
+
+    public Pessoa(String nome, String sobrenome){
+        this.nome = nome;
+        this.sobrenome= sobrenome;
+
+    }
+
+    
+}
